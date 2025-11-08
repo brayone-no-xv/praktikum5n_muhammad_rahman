94a5ef48fb40d1805cc6e6c5a45336f39e9015ef
brayone-no-xv <hrepunzel@gmail.com>
2025-11-08 13:43:16 +0800

    Frontend: Add include-name & class to submit Web P3 Description: - Sub-heading: Muhammad Rahman | 2310010325 | TI REG N BJM'23

--- web-praktikum/web-app/resources/views/admin/laporan/pdf_barang.blade.php
+++ web-praktikum/web-app/resources/views/admin/laporan/pdf_barang.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laporan Barang</title>
+    <title>Laporan Barang | M.RAHMAN</title>
 </head>
 <body>
-    <h1>laporan barang</h1>
+
+    <div style="text-align: center;">
+        <h1 style="text-transform: capitalize;">laporan barang</h1>
+                <h4>Muhammad Rahman | TI REG N BJM'23</h4>
+    </div>
+
     <table width="100%" cellpadding="10" cellspacing="0" border=1>
         <tr>
             <th>No</th>
@@ -25,5 +30,13 @@
             </tr>
         @endforeach
     </table>
+    <div>
+    </div>
+
+    <style>
+        tr > th {
+            background: yellow;
+        }
+    </style>
 </body>
 </html>
